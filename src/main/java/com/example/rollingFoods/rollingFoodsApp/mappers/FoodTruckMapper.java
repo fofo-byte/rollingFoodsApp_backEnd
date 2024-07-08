c9a9d64f3656183585c2dd0b2f8236a820441d9f
alfonso <alfonso.casucci@gmail.com>
2024-07-08 19:23:45 +0200

    commit updates config swagger ect

---
+++ src/main/java/com/example/rollingFoods/rollingFoodsApp/mappers/FoodTruckMapper.java
@@ -0,0 +1,11 @@
+package com.example.rollingFoods.rollingFoodsApp.mappers;
+
+
+import org.mapstruct.Mapping;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface FoodTruckMapper {
+}
